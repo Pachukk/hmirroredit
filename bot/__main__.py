5e76e315ac2cb88d2976c8fbb902354af390335a
Pachukk <88023964+Pachukk@users.noreply.github.com>
2022-07-07 10:17:12 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -57,8 +57,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/arshsisodiya/helios-mirror")
-    buttons.buildbutton("Support Group", "https://t.me/mirrorsociety")
+    buttons.buildbutton("🧞‍♂️Owner🧞‍♂️", "tg://settings/")
+    buttons.buildbutton("🖕Myran🖕", "https://t.me/njnmyr")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
